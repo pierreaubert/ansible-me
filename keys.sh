ab010afe176eb0578262a92670bc9347b38f01eb
Pierre F. Aubert <pierreaubert@yahoo.fr>
2014-05-26 16:07:50 +0200

    add keys to simplify keys creation with openssl

---
+++ keys.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+server=$1
+
+openssl genrsa -des3 -out $server.key 1024
+openssl req -new -newkey rsa:2048 -nodes -out $server.csr -keyout $server.key -subj '/C=FR/ST=Alpes Maritimes/L=Grasse/O=7pi/CN=7pi.eu'
+cp $server.key $server.key.org
+openssl rsa -in $server.key.org -out $server.key
+openssl x509 -req -days 365 -in $server.csr -signkey $server.key -out $server.crt
+
+mv $server.csr $server.crt $server.key* keys
