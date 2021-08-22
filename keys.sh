5300df1b13a24a7a7bde1d5c15dc1e8d6072c259
pierreaubert <pierre0aubert@gmail.com>
2021-08-22 13:47:28 +0000

    switch to new home

--- keys.sh
+++ keys.sh
@@ -3,7 +3,7 @@
 server=$1
 
 openssl genrsa -des3 -out $server.key 2048
-openssl req -new -newkey rsa:2048 -nodes -out $server.csr -keyout $server.key -subj '/C=FR/ST=Alpes Maritimes/L=Grasse/O=7pi/CN=7pi.eu'
+openssl req -new -newkey rsa:2048 -nodes -out $server.csr -keyout $server.key -subj '/C=CH/ST=Argau/L=Aarau/O=7pi/CN=7pi.ch'
 cp $server.key $server.key.org
 openssl rsa -in $server.key.org -out $server.key
 openssl x509 -req -days 365 -in $server.csr -signkey $server.key -out $server.crt
